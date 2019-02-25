769e3e62172a3098cd0700f1b2818172ff3c4aec
iliana <iliana000@gmail.com>
2019-02-25 22:22:42 +0200

    starnavi project

---
+++ precache-manifest.9c79cc4ddb63a89673fa1afa79dc2bb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "4c247d2c79f9ce0b737f",
+    "url": "/static/js/main.4c247d2c.chunk.js"
+  },
+  {
+    "revision": "f7dca1ff3614bf9b8af5",
+    "url": "/static/js/2.f7dca1ff.chunk.js"
+  },
+  {
+    "revision": "4c247d2c79f9ce0b737f",
+    "url": "/static/css/main.23833b5c.chunk.css"
+  },
+  {
+    "revision": "6d0212397606bc08913363491d9ce2ff",
+    "url": "/index.html"
+  }
+];
