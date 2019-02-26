d4c6dd7e67126ecc521179d1284e384c9a270a01
iliana <iliana000@gmail.com>
2019-02-26 22:47:45 +0200

    fixing path

---
+++ build/precache-manifest.357ce15c5688a17317dbed3f567d2477.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/build/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cb13a552592dfcf90b17",
+    "url": "/build/static/js/runtime~main.cb13a552.js"
+  },
+  {
+    "revision": "4b5cbcf332071b04c404",
+    "url": "/build/static/js/main.4b5cbcf3.chunk.js"
+  },
+  {
+    "revision": "84fbd139b5c102dafcc2",
+    "url": "/build/static/js/2.84fbd139.chunk.js"
+  },
+  {
+    "revision": "4b5cbcf332071b04c404",
+    "url": "/build/static/css/main.7222c5b6.chunk.css"
+  },
+  {
+    "revision": "147dc1f8296f7d6144ea8165d9ead865",
+    "url": "/build/index.html"
+  }
+];
