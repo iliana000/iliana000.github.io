982e45779a08e7d5ea7e1fd68afc019df4a2f33f
iliana <iliana000@gmail.com>
2019-02-26 14:22:47 +0200

    new files

---
+++ precache-manifest.6e99f24b795cbfcc73ae6b79e1613116.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e3d06daa3ff30a71ef81",
+    "url": "/static/js/main.e3d06daa.chunk.js"
+  },
+  {
+    "revision": "b3c63ab6c32bb0f8ed5b",
+    "url": "/static/js/2.b3c63ab6.chunk.js"
+  },
+  {
+    "revision": "e3d06daa3ff30a71ef81",
+    "url": "/static/css/main.c6b46f8a.chunk.css"
+  },
+  {
+    "revision": "10d3e79f3caebc404b7562924d1e391d",
+    "url": "/index.html"
+  }
+];
