a90aed14cfa11508902de3db99841d056cf3433c
iliana <iliana000@gmail.com>
2019-02-26 22:40:31 +0200

    build

---
+++ build/precache-manifest.2e37f420d69bb8e5e79c8b3a7adac7fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "fc55b04fedcefe528d3e",
+    "url": "/static/js/main.fc55b04f.chunk.js"
+  },
+  {
+    "revision": "84fbd139b5c102dafcc2",
+    "url": "/static/js/2.84fbd139.chunk.js"
+  },
+  {
+    "revision": "fc55b04fedcefe528d3e",
+    "url": "/static/css/main.c6b46f8a.chunk.css"
+  },
+  {
+    "revision": "3f47ee42797dbad7c40426d86fbee223",
+    "url": "/index.html"
+  }
+];
